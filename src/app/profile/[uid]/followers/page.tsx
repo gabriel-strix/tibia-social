f5eb645f0d6217a37cb93c5ef80c622e1f9859b6
gabriel-strix <gabriel.faria@live.com>
2025-07-10 19:05:31 -0300

    Tipagem seguidores

--- src/app/profile/[uid]/followers/page.tsx
+++ src/app/profile/[uid]/followers/page.tsx
@@ -1,11 +1,5 @@
 import FollowersListPage from "@/components/FollowersListPage";
 
-interface PageProps {
-  params: {
-    uid: string;
-  };
-}
-
-export default function ProfileFollowersPage({ params }: PageProps) {
+export default function ProfileFollowersPage({ params }) {
   return <FollowersListPage profileUid={params.uid} type="followers" />;
 }
