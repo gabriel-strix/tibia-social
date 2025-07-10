e6407bf66404e8871767342286a5c10e1990447b
gabriel-strix <gabriel.faria@live.com>
2025-07-10 18:49:45 -0300

    Tipagem

--- src/app/profile/[uid]/followers/page.tsx
+++ src/app/profile/[uid]/followers/page.tsx
@@ -1,6 +1,5 @@
 import FollowersListPage from "@/components/FollowersListPage";
 
-// Remover tipagem manual e usar a inferida pelo Next.js
-export default function ProfileFollowersPage({ params }: { params: { uid: string } }) {
+export default function ProfileFollowersPage({ params }) {
   return <FollowersListPage profileUid={params.uid} type="followers" />;
 }
